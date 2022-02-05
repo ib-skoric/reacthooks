fd4e1292f8ecebe08fb43ef914fd7db438a207a2
Ivan Branimir Skoric <ib.skoric@outlook.com>
2022-02-05 13:05:49 +0000

    Added functionality for highlighting stars

--- src/index.js
+++ src/index.js
@@ -7,15 +7,27 @@ const createArray = (length) =>
     [...Array(length)]
 ;
 
-const Star = ({ selected = false }) => {
+const Star = ({ selected = false, onSelect }) => {
     return(
-        <FaStar color={selected ? "red" : "gray"}/>
-    )
+        // add an onclick behaviour that calls the on select function from below, which updates the number of stars selected
+        <FaStar color={selected ? "red" : "gray"} onClick={onSelect}/>
+        )
 }
 
 // use total stars (props.totalStars) and if nothing is passed use 5 as default
 const StarRating = ({ totalStars = 5 }) => {
-    return createArray(totalStars).map((n,i) => (<Star key={i}/>))
+    const [selectedStars, setSelectedStars] = useState(0);
+    
+    return (
+        <>
+        {/*    Map over the array and create a star element for each and give it an iterator/index of i */}
+        {createArray(totalStars).map((n,i) => (<Star key={i}
+                                                       // check if number of selected stars is greater than iterator num, returns true or false
+                                                       selected={selectedStars > i}
+                                                       // pass the onselect as prop to the stars and add 1 to the selected stars if a star element is selected
+                                                        // this is because of 0 indexing
+                                                       onSelect={() => setSelectedStars((prevState) => i+1)}/>))}
+        </>)
 }
 
 function App() {
