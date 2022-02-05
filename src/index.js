b01f12b642e7f77b757ee7b75cd96e78c557b200
Ivan Branimir Skoric <ib.skoric@outlook.com>
2022-02-05 13:23:16 +0000

    Experiment with useState hooks

--- src/index.js
+++ src/index.js
@@ -1,38 +1,35 @@
-import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { FaStar } from 'react-icons/fa'
+import React, { useEffect, useState } from 'react';
 
-const createArray = (length) =>
-    [...Array(length)]
-;
-
-const Star = ({ selected = false, onSelect }) => {
-    return(
-        // add an onclick behaviour that calls the on select function from below, which updates the number of stars selected
-        <FaStar color={selected ? "red" : "gray"} onClick={onSelect}/>
-        )
-}
-
-// use total stars (props.totalStars) and if nothing is passed use 5 as default
-const StarRating = ({ totalStars = 5 }) => {
-    const [selectedStars, setSelectedStars] = useState(0);
-    
-    return (
-        <>
-        {/*    Map over the array and create a star element for each and give it an iterator/index of i */}
-        {createArray(totalStars).map((n,i) => (<Star key={i}
-                                                       // check if number of selected stars is greater than iterator num, returns true or false
-                                                       selected={selectedStars > i}
-                                                       // pass the onselect as prop to the stars and add 1 to the selected stars if a star element is selected
-                                                        // this is because of 0 indexing
-                                                       onSelect={() => setSelectedStars((prevState) => i+1)}/>))}
-        </>)
-}
 
 function App() {
+    
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch(`https://api.github.com/users`).then(response => response.json())
+        .then(response => setData(response));
+    }, [])
+
+    if (data) {
+        return(
+            <div>
+            <ul>
+                {data.map(user => (
+                    <li key={user.id}>
+                        {user.login}
+                    </li>
+                ))}
+            </ul>
+            <button onClick={() => setData([])}>Remove data</button>
+            </div>
+           
+        )
+    }
+
     return (
-        <StarRating totalStars={10}/>
+        <p>No users.</p>
     );
 }
 
